f6fcd8625013ccfd3874956c3a03c560f193a454
Rishabh-Kodes <rishabhkodes@gmail.com>
2021-11-22 18:24:54 +0530

    Added Side Nav

--- components/Layout/SideNav.js
+++ components/Layout/SideNav.js
@@ -1,5 +1,19 @@
+import { FaTwitter, FaFacebook, FaYoutube } from "react-icons/fa";
+
 const SideNav = () => {
-  return <div className="side-nav">Side Nav</div>;
+  return (
+    <div className="side-nav">
+      <div className="logo">
+        <img src="dalal-tiles.png" />
+      </div>
+
+      <div className="sm-links">
+        <FaTwitter />
+        <FaFacebook />
+        <FaYoutube />
+      </div>
+    </div>
+  );
 };
 
 export default SideNav;
