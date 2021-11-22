21a3721c837ccb08e596c8aa735ef50a57aa1c90
Rishabh-Kodes <rishabhkodes@gmail.com>
2021-11-22 18:38:34 +0530

    Added Navbar

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,5 +1,14 @@
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="nav-menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Products</li>
+        <li>Contact</li>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
