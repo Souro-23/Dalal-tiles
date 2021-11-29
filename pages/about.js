d8bc75f074ab22784b79d115b82d5ab343fec77a
Rishabh-Kodes <rishabhkodes@gmail.com>
2021-11-29 23:30:29 +0530

    Added About sidebar

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,9 @@
 import Layout from "@components/Layout";
+import AboutPageSidebar from "@section/AboutPageSidebar";
 
 const About = () => {
   return (
-    <Layout>
+    <Layout sidebar={<AboutPageSidebar />}>
       <video controls className="about-video">
         <source
           src="https://vod-progressive.akamaized.net/exp=1638128509~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F436%2F13%2F327180404%2F1281319886.mp4~hmac=6faa29895f01942f935b6f3617f18ffa333a60406c1cceb3225a75dfe3d249dc/vimeo-prod-skyfire-std-us/01/436/13/327180404/1281319886.mp4?download=1&filename=Pexels+Videos+2073129.mp4"
---
+++ sections/AboutPageSidebar.js
@@ -0,0 +1,23 @@
+import SidebarSlider from "@components/SidebarSlider";
+import { OurClients } from "_data/OurClients";
+
+const AboutPageSidebar = () => {
+  return (
+    <>
+      <button className="visit-btn">Book a visit</button>
+
+      <SidebarSlider heading="Featured Products" className="border-b" />
+
+      <div>
+        <div className="sidebar-heading">Our clients</div>
+        <div className="grid-3">
+          {OurClients.map((client, index) => (
+            <img src={client.logo} alt={client.name} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutPageSidebar;
