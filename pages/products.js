143c308f8231b90b40e411a01abe72824204e38b
Rishabh-Kodes <rishabhkodes@gmail.com>
2022-02-19 00:12:20 +0530

    Fixed horizontal slider

--- pages/products.js
+++ pages/products.js
@@ -1,13 +1,15 @@
-import { useRef } from "react";
-import { Swiper } from "swiper/react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 import { HiOutlineArrowLeft, HiOutlineArrowRight } from "react-icons/hi";
 import { Navigation } from "swiper";
 
-const HorizontalSlider = (props) => {
+const products = () => {
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
+
   return (
-    <div className="horizontal-slider">
+    <div className="products-page">
       <div className="navigation-buttons black prev-btn">
         <div ref={navigationPrevRef}>
           <HiOutlineArrowLeft />
@@ -37,10 +39,18 @@ const HorizontalSlider = (props) => {
           swiper.params.navigation.nextEl = navigationNextRef.current;
         }}
       >
-        {props.children}
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
     </div>
   );
 };
 
-export default HorizontalSlider;
+export default products;
--- sections/AccordionContents/Achievements.js
+++ sections/AccordionContents/Achievements.js
@@ -8,20 +8,14 @@ const Achievements = (props) => {
     <HorizontalSlider>
       {Testimonials.map((item, index) => (
         <SwiperSlide key={index} style={{ height: "100%" }}>
-          {({ isActive }) => (
-            <div
-              className="testimonial"
-              key={index}
-              style={isActive ? { opacity: 0 } : {}}
-            >
-              <div className="image">
-                <img src={item.image} alt={item.name} />
-              </div>
-              <p className="description">{item.description}</p>
-              <p className="name">{item.name}</p>
-              <p className="designation">{item.designation}</p>
+          <div className="testimonial" key={index}>
+            <div className="image">
+              <img src={item.image} alt={item.name} />
             </div>
-          )}
+            <p className="description">{item.description}</p>
+            <p className="name">{item.name}</p>
+            <p className="designation">{item.designation}</p>
+          </div>
         </SwiperSlide>
       ))}
     </HorizontalSlider>
