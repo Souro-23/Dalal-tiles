3ce8d5901a0bb1fe29114fe98dc84aa20e4287f6
Rishabh-Kodes <rishabhkodes@gmail.com>
2022-02-20 19:05:59 +0530

    Responsive sidebar for tablet

--- sections/HomePageSidebar.js
+++ sections/HomePageSidebar.js
@@ -2,12 +2,12 @@ import SidebarSlider from "@components/SidebarSlider";
 
 const HomePageSidebar = () => {
   return (
-    <div className="homepage-sidebar">
+    <>
       <button className="visit-btn">Book a visit</button>
 
       <SidebarSlider heading="Featured Products" className="border-b" />
       <SidebarSlider heading="Our Projects" />
-    </div>
+    </>
   );
 };
 
