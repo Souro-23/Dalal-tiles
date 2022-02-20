df76e3c5435113fc6814d899dfa104eb9f5863d2
Rishabh-Kodes <rishabhkodes@gmail.com>
2022-02-20 20:26:48 +0530

    Completed mobile layout responsiveness

---
+++ components/Layout/MoileNav.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { NavLink } from "./Navlink";
+import { FaPhoneAlt } from "react-icons/fa";
+import { RiMenu3Line } from "react-icons/ri";
+import { CgClose } from "react-icons/cg";
+
+const MobileNav = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <>
+      <div className="mobile-nav">
+        <div className="logo">
+          <img src="/dalal-tiles.png" />
+        </div>
+
+        <a href="#" className="visit-btn">
+          Book a visit
+        </a>
+        <div className="call-btn">
+          <FaPhoneAlt />
+        </div>
+
+        <RiMenu3Line
+          style={{ fontSize: "2rem" }}
+          onClick={() => setIsActive(true)}
+        />
+      </div>
+      <div className={isActive ? "mob-nav-menu active" : "mob-nav-menu"}>
+        <div className="close-btn" onClick={() => setIsActive(false)}>
+          <CgClose />
+        </div>
+        <NavLink href="/" exact className="nav-link">
+          Home
+        </NavLink>
+
+        <NavLink href="/about" className="nav-link">
+          About
+        </NavLink>
+
+        <NavLink href="/products" className="nav-link">
+          Products
+        </NavLink>
+
+        <NavLink href="/contact" className="nav-link">
+          Contact
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default MobileNav;
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,19 +2,22 @@ import SideNav from "@components/Layout/SideNav";
 import Navbar from "@components/Layout/Navbar";
 import CallNow from "@components/Layout/CallNow";
 import Footer from "@components/Layout/Footer";
-import HomePageSlider from "@section/HomePageSlider";
+import MobileNav from "./MoileNav";
 import HomePageSidebar from "@section/HomePageSidebar";
 
 const Layout = ({ children, sidebar }) => {
   return (
-    <div className="layout-container">
-      <SideNav />
-      <Navbar />
-      <CallNow />
-      <div className="main">{children}</div>
-      <div className="sidebar">{sidebar ? sidebar : <HomePageSidebar />}</div>
-      <Footer />
-    </div>
+    <>
+      <MobileNav />
+      <div className="layout-container">
+        <SideNav />
+        <Navbar />
+        <CallNow />
+        <div className="main">{children}</div>
+        <div className="sidebar">{sidebar ? sidebar : <HomePageSidebar />}</div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
