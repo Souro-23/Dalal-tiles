8d08f707413708a4ed632c5a227247f0bcf16cbf
Rishabh-Kodes <rishabhkodes@gmail.com>
2022-03-22 01:42:22 +0530

    Removed products page content

--- pages/products/index.js
+++ pages/products/index.js
@@ -50,21 +50,6 @@ const Products = () => {
             Our Freshly Updated Catalogue is out now. Get your digital copy to
             explore our whole range.
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do nim ad minim
-            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in
-            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est labo
-          </p>
 
           <Link href="/products/catalogue">
             <button className="btn btn-outline">Download Catalouge</button>
