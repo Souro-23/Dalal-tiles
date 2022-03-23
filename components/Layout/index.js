92a03bece40be8c6d64b305e2892203dab218958
Rishabh-Kodes <rishabhkodes@gmail.com>
2022-03-23 10:40:40 +0530

    Added Meta tags

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,10 +4,12 @@ import CallNow from "@components/Layout/CallNow";
 import Footer from "@components/Layout/Footer";
 import MobileNav from "./MoileNav";
 import HomePageSidebar from "@section/HomePageSidebar";
+import Meta from "@components/Meta";
 
 const Layout = ({ children, sidebar }) => {
   return (
     <>
+      <Meta />
       <MobileNav />
       <div className="layout-container">
         <SideNav />
---
+++ components/Meta.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+      <title>Dalal Tiles</title>
+      <meta name="title" content="Dalal Tiles" />
+      <meta
+        name="description"
+        content="Dalal Tiles has its history owed to its pioneers Mr. Anil Dalal and Mr. Rakesh Dalal that have instilled the values of transparency, Honesty, and excelling quality to build relationships towards an innovative and creativity-driven community of Architects and Engineers. Ensuring the promise of delivering excellence, Dalal Tiles Pvt. Ltd. extends the execution, application, and installation of products and all services related, to Dalal Builders Pvt. Ltd. creating an umbrella of solutions under the Dalal Tiles Banner."
+      />
+      <meta
+        name="keywords"
+        content="Dala Tiles, Tiles, Building Products, Construction, Landscape Products, Urban Accessories, Wall Tiles, Green Building Products, Architectural Elements, Concrete Pavers"
+      />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English"></meta>
+    </Head>
+  );
+};
+
+export default Meta;
