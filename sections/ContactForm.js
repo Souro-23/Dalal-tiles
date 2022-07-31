4d911ff6aa06204aeae63a34f67164b6da3fcb27
sourodeep23 <98368233+sourodeep23@users.noreply.github.com>
2022-07-31 09:21:52 +0530

    UI fixes according to small screenm

--- sections/ContactForm.js
+++ sections/ContactForm.js
@@ -13,10 +13,10 @@ const ContactForm = () => {
       <div className="form-group">
         <label htmlFor="name">I am a..</label>
         <div className="grid">
-          <div onClick={()=>{setUserType("Home Owner")}} style={userType==="Home Owner"?{border:"2px solid black"}:{}} className="block-content">
+          <div onClick={() => { setUserType("Home Owner") }} style={userType === "Home Owner" ? { border: "2px solid black" } : {}} className="block-content">
             <p >Home Owner</p>
           </div>
-          <div onClick={()=>{setUserType("Professional")}} style={userType==="Professional"?{border:"2px solid black"}:{}} className="block-content">
+          <div onClick={() => { setUserType("Professional") }} style={userType === "Professional" ? { border: "2px solid black" } : {}} className="block-content">
             <p >Professional</p>
           </div>
         </div>
@@ -24,10 +24,10 @@ const ContactForm = () => {
       <div className="form-group">
         <label htmlFor="name">Are you a new customer</label>
         <div className="grid">
-          <div style={!oldUser?{border:"2px solid black"}:{}}  onClick={()=>{setOldUser(false)}} className="block-content">
+          <div style={oldUser===false ? { border: "2px solid black" } : {}} onClick={() => { setOldUser(false) }} className="block-content">
             <p>Yes</p>
           </div>
-          <div style={oldUser?{border:"2px solid black"}:{}}   onClick={()=>{setOldUser(true)}} className="block-content">
+          <div style={oldUser===true ? { border: "2px solid black" } : {}} onClick={() => { setOldUser(true) }} className="block-content">
             <p>No</p>
           </div>
         </div>
@@ -35,17 +35,18 @@ const ContactForm = () => {
       <div className="form-group">
         <label htmlFor="name">Enter Your Details</label>
         <div className="grid">
-          <input type="text" name="firstName" required placeholder="First Name" onChange={(e)=>{setFirstName(e.target.value)}} />
-          <input type="text" name="lastName" required placeholder="Last Name" onChange={(e)=>{setLastName(e.target.value)}} />
+          <input type="text" name="firstName" required placeholder="First Name" onChange={(e) => { setFirstName(e.target.value) }} />
+          <input type="text" name="lastName" required placeholder="Last Name" onChange={(e) => { setLastName(e.target.value) }} />
         </div>
       </div>
       <div className="form-group">
         {/* <label htmlFor="email">Email</label> */}
-        <input type="email" name="email" required placeholder="Enter Your E-Mail" onChange={(e)=>{setEmail(e.target.value)}} />
-      </div>
-      <div className="form-group">
-        {/* <label htmlFor="number">Number</label> */}
-        <input type="text" name="number" required placeholder="Enter Your Phone Number" onChange={(e)=>{setPhoneNo(e.target.value)}}/>
+        <div className="grid">
+          <input type="email" name="email" required placeholder="Enter Your E-Mail" onChange={(e) => { setEmail(e.target.value) }} />
+          <input type="text" name="number" required placeholder="Enter Your Phone Number" onChange={(e) => { setPhoneNo(e.target.value) }} />
+
+        </div>
+
       </div>
       {/* <div className="form-group">
         <label htmlFor="user-type">Who you are?</label>
@@ -58,7 +59,7 @@ const ContactForm = () => {
       </div> */}
       <div className="form-group">
         {/* <label htmlFor="message">Your message</label> */}
-        <textarea name="message" rows="5" placeholder="Your Message" onChange={(e)=>{setMessage(e.target.value)}}></textarea>
+        <textarea name="message" rows="2" placeholder="Your Message" onChange={(e) => { setMessage(e.target.value) }}></textarea>
       </div>
       <input type="button" value="Submit" className="submit-btn" />
     </from>
