6f375d87d957095444d03d3d0dbae566dbb2e0bb
sourodeep23 <98368233+sourodeep23@users.noreply.github.com>
2022-11-23 08:34:57 +0530

    link added

--- components/Layout/SideNav.js
+++ components/Layout/SideNav.js
@@ -1,19 +1,39 @@
-import { FaTwitter, FaFacebook, FaYoutube } from "react-icons/fa";
-import { useRouter } from 'next/router'
-
+import { FaTwitter, FaFacebook, FaLinkedinIn } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 const SideNav = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div className="side-nav">
-      <div onClick={()=>router.push('/')} className="logo">
+      <div onClick={() => router.push("/")} className="logo">
         <img src="/dalal-tiles.png" />
       </div>
 
       <div className="sm-links">
-        <FaTwitter />
-        <FaFacebook />
-        <FaYoutube />
+        <a
+          href="https://mobile.twitter.com/dalal_tiles?lang=en"
+          target="_blank"
+          rel="noreferrer"
+          className="detail-btn"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://www.facebook.com/dalaltiles"
+          target="_blank"
+          rel="noreferrer"
+          className="detail-btn"
+        >
+          <FaFacebook />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/dalal-tiles/"
+          target="_blank"
+          rel="noreferrer"
+          className="detail-btn"
+        >
+          <FaLinkedinIn />
+        </a>
       </div>
     </div>
   );
